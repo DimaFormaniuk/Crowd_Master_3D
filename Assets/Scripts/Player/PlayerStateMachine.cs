5827d0e637eaeb4ff0e8f7442d357a07a361402a
DimaFormaniuk <44363635+DimaFormaniuk@users.noreply.github.com>
2021-02-03 13:40:01 +0200

    lesson 22

--- Assets/Scripts/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-[RequireComponent(typeof(Rigidbody), typeof(Animator),typeof(HealthContainer))]
+[RequireComponent(typeof(Rigidbody), typeof(Animator), typeof(HealthContainer))]
 public class PlayerStateMachine : MonoBehaviour
 {
     [SerializeField] private State _firstState;
@@ -11,6 +12,8 @@ public class PlayerStateMachine : MonoBehaviour
     private Rigidbody _rigidbody;
     private Animator _animator;
     private HealthContainer _health;
+
+    public event UnityAction Damage;
 
     private void OnEnable()
     {
@@ -65,6 +68,7 @@ public class PlayerStateMachine : MonoBehaviour
 
     public void ApplyDamage(float damage)
     {
+        Damage?.Invoke();
         _health.TakeDamage((int)damage);
     }
 }
